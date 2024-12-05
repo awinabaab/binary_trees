4ae133f6ce1931923c31f276a2855aa0f29f6a5d
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-12-05 17:07:53 +0000

    Conform to Betty code style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,7 +8,8 @@
  *
  * Return: Pointer to the lowest common ancestor, otherwise NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	const binary_tree_t *first_node;
 	const binary_tree_t *second_node;
